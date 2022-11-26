16eeee6e8f28e5aa054ca66ee8ab9e68d24bd37c
s0shadow0BG <118588339+s0shadow0BG@users.noreply.github.com>
2022-11-26 08:46:27 +0200

    Update hack.lua

--- hack.lua
+++ hack.lua
@@ -1,1 +1,45 @@
+-- REQUIRED
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("TITLE", "DarkTheme")
 
+--TABS
+
+local main = Window:NewTab("MAIN")
+
+local player = Window:NewTab("Player")
+
+local credits = Window:NewTab("Credits")
+
+--SECTIONS
+
+local PlayerSection = player:NewSection("Player")
+
+local MainSection = main:NewSection("Main")
+
+local CreditsSection = credits:NewSection("Credits")
+
+--LABELS
+
+CreditsSection:NewLabel("By s0shadow0BG#4639(DM FOR BUGS)")
+
+--BUTT0NS
+
+--TOGGLES
+
+--SLIDERS
+
+--TEXTBOXES
+
+--KEYBINDS
+
+--TOGGLING UI WITH KEYBINDS
+
+--DROPDOWNS
+
+--COLOR PICKER
+
+--SCRIPTS
+
+--_G
+
+--OTHER
